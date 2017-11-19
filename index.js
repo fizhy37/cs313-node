e0ef92c8a4f49407417d2eddc6889295c9c1f2aa
fizhy37 <fis15028@byui.edu>
2017-11-18 17:52:03 -0700

    hopefully queries correctly

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT * FROM item;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
