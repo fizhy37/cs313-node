63e4e3ae027b91fb8062a73ff3dd36efbf304575
fizhy37 <fis15028@byui.edu>
2017-11-18 18:51:27 -0700

    trying again

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const client = new Client({
   ssl: true,
 });
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
